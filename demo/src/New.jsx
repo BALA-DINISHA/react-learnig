5833f640818493fa63123c506c988eba87ecd99c
BALA-DINISHA <baladinisha@gmail.com>
2026-08-14 12:42:33 +0530

    add new docker file

--- demo/src/New.jsx
+++ demo/src/New.jsx
@@ -1,11 +1,16 @@
 import  {useState, useEffect} from 'react';
 import axios from 'axios';
+
+
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4444';
+
 function UserList() {
     const [users, setUsers] = useState([]);
     const [loading, setLoading]= useState(true);
     const [error, setError]=useState(null);
+
     useEffect(() => {
-        axios.get('http://localhost:4444/api/people')
+        axios.get(`${API_URL}/api/peopl`)
         .then((Response) => {
             setUsers(Response.data);
         }).catch((err)=>{
