36e0e087ba975695a1e23c6bc66683786f68b8eb
BALA-DINISHA <baladinisha@gmail.com>
2026-08-14 12:09:50 +0530

    add new docker file

--- spring_learning/src/main/java/com/example/spring_learning/CorsConfig.java
+++ spring_learning/src/main/java/com/example/spring_learning/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://vercel.com/bala-dinishas-projects/react-learnig")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
